802d0827a1a178e5b77433604a7f69d7e5d5fa99
Alexandru Ion <elijah@Alexandrus-MacBook-Pro.local>
2021-12-30 12:10:16 +0200

    d

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,16 +1,15 @@
 import NextAuth from "next-auth";
-import Googlerovider from "next-auth/providers/google";
+import GoogleProvider from "next-auth/providers/google";
 
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
-    Googlerovider({
+    GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
     // ...add more providers here
   ],
-  secret: process.env.SECRET,
 
   pages: {
     signIn: "/auth/signin",
